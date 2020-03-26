075cdaae183b42506c3ceabb49de7bef29b40da7
Petra <petra.stumpf@gmail.com>
2020-03-26 13:43:29 -0700

    add caps, bold font, right alignment to navbar

--- components/EccNav.vue
+++ components/EccNav.vue
@@ -1,20 +1,29 @@
 <template>
-  <b-navbar toggleable="md" type="dark" class="navbar">
-    <b-navbar-brand href="#">
-      ECC
-    </b-navbar-brand>
+  <b-navbar
+    id="mainNav"
+    toggleable="md"
+    type="dark"
+    class="navbar text-uppercase fixed-top navbar-shrink"
+  >
+    <b-navbar-brand class="navbar-brand" href="#">ECC</b-navbar-brand>
     <b-navbar-toggle target="nav-collapse" />
-    <b-collapse id="nav-collapse" is-nav>
-      <b-navbar-nav>
-        <b-nav-item href="#about">
-          About
-        </b-nav-item>
-        <b-nav-item href="#events">
-          Events
-        </b-nav-item>
-        <b-nav-item href="#resources">
-          Resources
-        </b-nav-item>
+    <b-collapse id="nav-collapse" is-nav class="text-uppercase">
+      <b-navbar-nav id="navbarResponsive" class="ml-auto">
+        <b-nav-item
+          class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger"
+          href="#events"
+          >Events</b-nav-item
+        >
+        <b-nav-item
+          class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger"
+          href="#about"
+          >About</b-nav-item
+        >
+        <b-nav-item
+          class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger"
+          href="#resources"
+          >Resources</b-nav-item
+        >
       </b-navbar-nav>
     </b-collapse>
   </b-navbar>

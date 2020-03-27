85846e984509cacf8211e16472da76be42178aea
Petra <petra.stumpf@gmail.com>
2020-03-26 19:35:42 -0700

    more nav styling

--- components/EccNav.vue
+++ components/EccNav.vue
@@ -3,29 +3,31 @@
     id="mainNav"
     toggleable="md"
     type="dark"
-    class="navbar text-uppercase fixed-top navbar-shrink"
+    class="navbar navbar-expand-lg text-uppercase fixed-top navbar-shrink bg-secondary"
   >
-    <b-navbar-brand class="navbar-brand" href="#">ECC</b-navbar-brand>
-    <b-navbar-toggle target="nav-collapse" />
-    <b-collapse id="nav-collapse" is-nav class="text-uppercase">
-      <b-navbar-nav id="navbarResponsive" class="ml-auto">
-        <b-nav-item
-          class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger"
-          href="#events"
-          >Events</b-nav-item
-        >
-        <b-nav-item
-          class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger"
-          href="#about"
-          >About</b-nav-item
-        >
-        <b-nav-item
-          class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger"
-          href="#resources"
-          >Resources</b-nav-item
-        >
-      </b-navbar-nav>
-    </b-collapse>
+    <div class="container">
+      <b-navbar-brand class="navbar-brand" href="#">ECC</b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse" />
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav id="navbarResponsive" class="ml-auto">
+          <b-nav-item
+            class="nav-link py-3 px-0 px-lg-3 rounded text-left"
+            href="#events"
+            >Events</b-nav-item
+          >
+          <b-nav-item
+            class="nav-link py-3 px-0 px-lg-3 rounded text-left"
+            href="#about"
+            >About</b-nav-item
+          >
+          <b-nav-item
+            class="nav-link py-3 px-0 px-lg-3 rounded text-left"
+            href="#resources"
+            >Resources</b-nav-item
+          >
+        </b-navbar-nav>
+      </b-collapse>
+    </div>
   </b-navbar>
 </template>
 

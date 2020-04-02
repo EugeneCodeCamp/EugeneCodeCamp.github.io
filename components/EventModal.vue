70cae516f410f4faebe21d96bf93e0eb5e3dfa54
Petra <petra.stumpf@gmail.com>
2020-04-02 16:41:06 -0700

    style footer button and remove modal body padding

--- components/EventModal.vue
+++ components/EventModal.vue
@@ -1,36 +1,36 @@
 <template>
   <b-modal
     :id="'EventModal' + event.id"
-    class="portfolio-modal modal fade bg-secondary"
+    class="sm-secondary"
     tabindex="-1"
     role="dialog"
-    aria-labelledby="portfolioModal1Label"
+    aria-labelledby="EventModal"
+    ok-only
+    button-size="lg"
   >
     <div class="modal-dialog modal-xl" role="document">
-      <div class="modal-content">
-        <div class="modal-body text-center">
-          <div class="row justify-content-center">
-            <div class="col-lg-8">
-              <!-- Portfolio Modal - Title -->
-              <h2
-                class="portfolio-modal-title text-secondary text-uppercase mb-0"
-              >
-                {{ event.title }}
-              </h2>
-              <!-- Icon Divider -->
-              <div class="divider-custom">
-                <div class="divider-custom-line"></div>
-                <div class="divider-custom-icon">
-                  <font-awesome-icon :icon="['fas', 'star']" />
-                </div>
-                <div class="divider-custom-line"></div>
+      <div class="text-center">
+        <div class="row justify-content-center">
+          <div class="col-lg-8">
+            <!-- Portfolio Modal - Title -->
+            <h2
+              class="portfolio-modal-title text-secondary text-uppercase mb-0"
+            >
+              {{ event.title }}
+            </h2>
+            <!-- Icon Divider -->
+            <div class="divider-custom">
+              <div class="divider-custom-line"></div>
+              <div class="divider-custom-icon">
+                <font-awesome-icon :icon="['fas', 'star']" />
               </div>
-              <!-- Portfolio Modal - Text -->
-              <p class="mb-5">{{ event.description }}</p>
-              <p class="mb-5">
-                <a :href="event.eventUrl" target="_new">More Information</a>
-              </p>
+              <div class="divider-custom-line"></div>
             </div>
+            <!-- Portfolio Modal - Text -->
+            <p class="mb-5">{{ event.description }}</p>
+            <p class="mb-5">
+              <a :href="event.eventUrl" target="_new">More Information</a>
+            </p>
           </div>
         </div>
       </div>

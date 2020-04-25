3327961d93931e9d42a9077295b4b2f356db7eca
Petra <petra.stumpf@gmail.com>
2020-04-25 12:41:06 -0700

    add a page for resources

---
+++ pages/resources.vue
@@ -0,0 +1,176 @@
+<template>
+  <!-- Resources Section -->
+  <section id="resources" class="page-section">
+    <div class="container">
+      <!-- Resources Section Heading -->
+      <h2
+        class="page-section-heading text-center text-uppercase text-secondary mb-0"
+      >
+        Resources
+      </h2>
+
+      <!-- Icon Divider -->
+      <div class="divider-custom">
+        <div class="divider-custom-line" />
+        <div class="divider-custom-icon">
+          <font-awesome-icon :icon="['fas', 'star']" />
+        </div>
+        <div class="divider-custom-line" />
+      </div>
+
+      <!-- Find Us Section -->
+      <div class="row">
+        <div class="col-md-6 col-lg-4">
+          <ul>
+            <li>
+              <a href="https://www.freecodecamp.org">freeCodeCamp</a> -
+              Responsive web design, javascript, front end libraries, data
+              visualization, information security and more!
+            </li>
+            <li>
+              <a href="https://www.codecademy.com">Codecademy</a> - HTML, CSS,
+              Python, JavaScript, SQL, Ruby, PHP and more!
+            </li>
+            <li>
+              <a href="https://www.theodinproject.com">The Odin Project</a> -
+              tracks for Full Stack Ruby on Rails, Full Stack JS, and Front End
+              Only
+            </li>
+            <li>
+              <a href="https://www.udemy.com/the-web-developer-bootcamp/"
+                >The Web Developer Bootcamp</a
+              >
+              - HTML, CSS, JS, Node, and More!
+            </li>
+            <li>
+              <a href="https://developer.mozilla.org/en-US/docs/Learn"
+                >Mozilla Development Network</a
+              >
+              - HTML, CSS, JS documentation, examples, and tutorials!
+            </li>
+            <li>
+              <a href="https://fullstackopen.com">Full Stack Open 2019</a> -
+              Learn React, Redux, Node.js, MongoDB, and GraphQL in one go! This
+              course will introduce you to modern JavaScript-based web
+              development. The main focus is on building single page
+              applications with ReactJS that use REST APIs built with Node.js.
+            </li>
+            <li>
+              <a href="https://www.100daysofcode.com/">100 Days of Code</a> -
+              Commit to 100 Days of Code!
+            </li>
+          </ul>
+        </div>
+        <div class="col-md-6 col-lg-4">
+          <ul>
+            <li>
+              Udemy Courses by Jonas Schmedtmann (low cost bootcamp-style
+              classes from a top teacher):
+              <ul>
+                <li>
+                  <a
+                    href="https://www.udemy.com/design-and-develop-a-killer-website-with-html5-and-css3/"
+                    >Build Responsive Real World Websites with HTML5 and CSS3</a
+                  >
+                </li>
+                <li>
+                  <a href="https://www.udemy.com/advanced-css-and-sass/">
+                    Advanced CSS and Sass: Flexbox, Grid, Animations and More!
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.udemy.com/the-complete-javascript-course/"
+                  >
+                    The Complete JavaScript Course 2019: Build Real Projects!
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.udemy.com/nodejs-express-mongodb-bootcamp/"
+                  >
+                    Node.js, Express, MongoDB &amp; More: The Complete Bootcamp
+                    2019
+                  </a>
+                </li>
+                <li>
+                  <a href="https://www.udemy.com/course/web-design-secrets/"
+                    >Web Design for Web Developers: Build Beautiful Websites!</a
+                  >
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a
+                href="https://www.edx.org/course/cs50s-introduction-to-computer-science"
+                >CS50 at Harvard University</a
+              >
+              - An introduction to the intellectual enterprises of computer
+              science and the art of programming.
+            </li>
+          </ul>
+        </div>
+        <div class="col-md-6 col-lg-4">
+          <ul>
+            <li>
+              Eugene-Area Coding Meetups:
+              <ul>
+                <li>
+                  <a href="http://eugenewebdevs.com">Eugene Web Devs</a> - A
+                  monthly meetup for those interested in web development.
+                </li>
+                <li>
+                  <a href="https://www.meetup.com/Elm-Eug/"
+                    >Elm Eugene / Funky Ducks</a
+                  >
+                  - For everything functional programming! (Also a fun and
+                  educational Friday lunch hangout.)
+                </li>
+                <li>
+                  <a href="https://www.meetup.com/pyeugene/">PyEugene</a> - A
+                  monthly meetup for those interested in the Python programming
+                  language.
+                </li>
+                <li>
+                  <a
+                    href="https://www.meetup.com/Eugene-Area-SQL-Server-User-Group/"
+                    >Eugene Area SQL Server User Group</a
+                  >
+                  - An official PASS chapter monthly meetup for SQL Server
+                  users.
+                </li>
+                <li>
+                  <a
+                    href="https://www.meetup.com/Technology-Association-of-Oregon-in-greater-Eugene/"
+                    >Eugene ISSA</a
+                  >
+                  - A monthly presentation for cybersecurity professionals and
+                  students.
+                </li>
+                <li>
+                  <a href="https://www.meetup.com/Corvallis-WordPress-Meetup/"
+                    >Corvallis WordPress Meetup</a
+                  >
+                  - A group of local WordPress developers, designers, and
+                  publishers who get together to share knowledge and experience.
+                </li>
+                <li>
+                  <a href="https://www.meetup.com/Eugene-Cybersecurity/"
+                    >Eugene Cybersecurity</a
+                  >
+                  - A meetup to talk IT Cybersecurity and related issues.
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
